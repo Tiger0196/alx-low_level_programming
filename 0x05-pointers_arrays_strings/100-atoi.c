ea13b7be77b918c48358e4e18ba78b09a543167b
Tiger0196 <michaeltiger0@gmail.com>
2023-09-23 08:15:19 -0700

    deferred

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,19 @@
 
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[c])
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] !='\0')
+		len++;
+	while (i < len && f == 0)
 	{
-	if (s[c] == 45)
-	{
-	min *= -1;
-	}
-	while (s[c] >= 48 && s[c] <= 57)
-	{
-	isi = 1;
-	ni = (ni * 10) + (s[c] - '0');
-	c++;
-	}
-	if (isi == 1)
-	{
-	break;
-	}
-	c++;
-	}
-	ni *= min;
-	return (ni);
-}
+		if (s[i] == ',')
+			++d;
+		if
